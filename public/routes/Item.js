3dec92dbf60d0de967a9bd0c040110d7c1109cc3
curious jeager <edam7635@gmail.com>
2019-04-07 00:51:56 +0900

    themeprovider and attrs pratice

--- public/routes/Item.js
+++ public/routes/Item.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import {Route, Link} from 'react-router-dom';
+import styled, {createGlobalStyle, css, ThemeProvider} from 'styled-components';
+import theme from '../styled-component/theme';
 
-const Item = ()=>{
-	return (
-		<div>
-			<p>Item</p>
-		</div>
-	);
+const GlobalStyle = createGlobalStyle`
+	body{
+		color:${props=>(props.whiteColor?'white':'black')};
+	}
+`
+
+const awesomeCard = css`
+	box-shadow:0 4px 6px rgba(5,5,9,0.11), 0 1px 3px rgba(0,0,0,0.08);
+	background-color:white;
+	border-radius:10px;
+	padding:10px;
+	`;
+
+const Input = styled.input.attrs({
+	required:true
+})`
+-webkit-appearance: none;
+  &:active,
+  &:focus{
+    outline:none;
+  }
+border:none;
+border-radius:5px;
+background-color:#fcf1ef;
+${awesomeCard};
+`;
+
+const Container = styled.body`
+  height:100px;
+  width:100%;
+  background-color:pink;
+`;
+
+const Card = styled.div`
+  background-color:white;
+`;
+
+const Button = styled.button`
+  border-radius:30px;
+  padding:25px 15px;
+  background-color:${props=>props.theme.successColor};
+`;
+
+class Item extends Component {
+	render(){
+		return (
+			<div>
+			<Input placeholder="Item"/>
+			<Container>
+			</Container>
+			<ThemeProvider theme={theme}>
+			<Form/>
+			</ThemeProvider>
+			</div>
+		);
+	}
 };
+
+const Form =()=>(<Card><Button>Item</Button></Card>);
 
 export default Item;
---
+++ public/styled-component/theme.js
@@ -0,0 +1,7 @@
+const theme ={
+    mainColor:"#e84393",
+    dangerColor:"#636e72",
+    successColor:"#81ecec"
+};
+
+export default theme;
